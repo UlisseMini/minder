cd52081c8c4fe4f09b1cd0c691a9dfdac99bd464
Ulisse Mini <ulisse.mini@gmail.com>
2021-04-07 17:41:30 -0400

    Update database for problems

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 # NOTE: When you add problems, use relationships and foreign keys
@@ -12,5 +13,15 @@ class User(Base):
     hashed_password = Column(String)
     bio = Column(String)
 
+    problems = relationship("Problem", back_populates="owner")
 
 
+
+class Problems(Base):
+    __tablename__ = "problems"
+
+    id = Column(Integer, primary_key=True)
+    tex = Column(String)
+    author_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="problems")
