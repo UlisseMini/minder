9ea3a3786286e3d51c8e108f89a5c4e12c60e479
Ulisse Mini <ulisse.mini@gmail.com>
2021-04-08 12:54:43 -0400

    Add type annotation to get_db

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 import sys
 
@@ -23,7 +24,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # Dependency
-def get_db():
+def get_db() -> Session:
     db = SessionLocal()
     try:
         yield db
