91a62babfff19b81e8e9294158a2a801dceebe24
Ulisse Mini <ulisse.mini@gmail.com>
2021-04-25 18:41:30 -0400

    Remove postgres comment

--- database.py
+++ database.py
@@ -14,7 +14,6 @@ if 'pytest' in sys.modules:
 
     SQLALCHEMY_DATABASE_URL = "sqlite:///" + temppath
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
