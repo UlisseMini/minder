a7bc31ded9bb7da4792c0e3a6dac6612131fcaaa
Ulisse Mini <ulisse.mini@gmail.com>
2021-04-25 18:34:10 -0400

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,19 @@ module.exports = {
     hash: true,
     template: './src/index.html',
   })],
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ],
+  }
 };
